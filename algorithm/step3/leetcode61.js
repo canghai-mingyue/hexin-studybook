58df72a89b22797bd61ff27be889543c1a862d87
canghai-mingyue <939877376@qq.com>
2021-11-05 01:50:56 +0800

    旋转链表的两种思路与做法

--- algorithm/step3/leetcode61.js
+++ algorithm/step3/leetcode61.js
@@ -28,6 +28,8 @@ let rotateRight = function(head, k) {
     let kk = k % length;
     // 处理特殊情况
     if(kk === 0) return head;
+
+
     // 连接成环
     a.next = head;
     // 需要断开的节点距离头节点的距离
@@ -42,3 +44,34 @@ let rotateRight = function(head, k) {
     head.next = null;
     return res;
 };
+
+// 快慢指针   向右移k位，即向右移 k%length = kk 位，也就是将后面kk位移到链表前面。
+// 先让快指针右移kk位，此时同时移动快慢指针，则当快指针到达链表尾部时，慢指针到达需要断开的节点，此时再将慢指针后面的链表移到前面
+let rotateRight = function(head, k) {
+    if(!head || !head.next) return head;
+    let a = head,
+        length = 1;
+    while(a.next) {
+        length++;
+        a = a.next;
+    }
+    let kk = k % length;
+    if(kk === 0) return head;
+    // 在这之前与上一个思路一样
+
+    let slow = head,
+        fast =head;
+    while(kk > 0) {
+        fast = fast.next;
+        kk--;
+    }
+    while(fast.next){
+        fast = fast.next;
+        slow = slow.next;
+    }
+    // 此时慢指针在断开点，快指针在链表尾部
+    let res = slow.next;
+    fast.next = head;
+    slow.next = null;
+    return res
+}
