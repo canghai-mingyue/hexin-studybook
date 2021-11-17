1747669eba166cf2add32fe20fd5aa40ebe575d5
canghai-mingyue <939877376@qq.com>
2021-11-18 02:14:54 +0800

    map复制带随机指针链表的更简单的理解

--- algorithm/step9/leetcode138.js
+++ algorithm/step9/leetcode138.js
@@ -19,17 +19,40 @@ const map = new Map();
 var copyRandomList = function(head) {
     if(!head) return head;
     if(!map.has(head)) {
-        // 此处必须先添加map的值，否则可能会陷入死循环。因为后面使用 copyRandomList 方法，可能会使用到
+        // 此处必须先添加map的值，否则可能会陷入死循环。因为后面使用 copyRandomList 方法，可能会使用到map
         map.set(head, new Node(head.val));
         Object.assign(map.get(head), { next: copyRandomList(head.next), random: copyRandomList(head.random) })
     }
     return map.get(head)
 };
+// 更好理解的map，两遍遍历，上面的递归就是这个的简化
+var copyRandomList = function(head) {
+    if(!head) return head;
+    let node = head;
+    // 第一次遍历， 创建新节点，但先不对 next 与 random 赋值
+    while(node !== null){
+        map.set(node, new Node(node.val, null, null))
+        node = node.next
+    }
+    node = head;
+    //  第二次遍历，为新的节点的 next 与 random 赋值
+    while(node !== null) {
+        let newNode = map.get(node);
+        // 这里 map.get(node.next) 和 map.get(node.random) 才是新的节点的 next 与 random
+        if(node.next !== null) newNode.next = map.get(node.next);
+        if(node.random !== null) newNode.random = map.get(node.random);
+        node = node.next
+    }
+    return map.get(head)
+};
+
+
+
 
 // 迭代与节点拆分  这个还是得配合图片更好理解一些
 var copyRandomList = function(head) {
     if(!head) return head;
-    // 首先新建复制节点
+    // 首先新建复制节点与原节点相连
     let node = head;
     while(node){
         let copyNode = new Node(node.val, node.next, null);
