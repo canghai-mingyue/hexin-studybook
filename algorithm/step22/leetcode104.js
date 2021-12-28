b5c7cca9440f47447369be1427266bf944a0e07a
canghai-mingyue <939877376@qq.com>
2021-12-28 23:58:39 +0800

    先把step23、24、25的题目记录下来

--- algorithm/step22/leetcode104.js
+++ algorithm/step22/leetcode104.js
@@ -12,8 +12,26 @@
  * @param {TreeNode} root
  * @return {number}
  */
-// 递归
+// DFS 递归
 var maxDepth = function(root) {
     if(!root) return 0;
     return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+};
+
+// BFS 当某一层的节点数量为 0 时，即得到最大深度
+var maxDepth = function(root) {
+    if(!root) return 0;
+    let queue = [root],
+        depth = 0;
+    while(queue.length > 0) {
+        depth++;
+        let length = queue.length;
+        for(let i = 0; i < length; i++) {
+            // 这里本层节点从头部移除，而下层节点从尾部推入
+            let tem = queue.shift();
+            tem.left && queue.push(tem.left)
+            tem.right && queue.push(tem.right)
+        }
+    }
+    return depth;
 };
