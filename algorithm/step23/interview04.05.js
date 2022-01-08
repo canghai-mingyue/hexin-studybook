6405a9d45091be05bd56c18e0f6d961078757216
canghai-mingyue <939877376@qq.com>
2022-01-08 12:56:20 +0800

    merge

--- algorithm/step23/interview04.05.js
+++ algorithm/step23/interview04.05.js
@@ -1,4 +1,12 @@
 // 面试题 04.05. 合法二叉搜索树
+/**
+ * Definition for a binary tree node.
+ */
+function TreeNode(val, left, right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+}
 
 /**
  * Definition for a binary tree node.
@@ -51,6 +59,19 @@ var isValidBST = function(root) {
     return true
 };
 
+// 中序遍历递归写法：
+// 有点奇怪，leetcode 测试用例为 [0] 时，说是输出为 false，但我自己测试输出却是正常的 true。不知道有没有可能是 leetcode 搞错了
+let prev = -Infinity
+var isValidBST = function(root) {
+    if(!root) return true;
+    if(!isValidBST(root.left)) return false;
+    if(root.val <= prev) return false;
+    prev = root.val;
+    return isValidBST(root.right);
+};
+
+let flag = isValidBST(new TreeNode(0, new TreeNode(-1), new TreeNode(2)))
+console.log(flag)
 
 //  还可以递归
 var isValidBST = function(root) {
@@ -63,3 +84,4 @@ var isValidBST = function(root) {
     }
     return helper(root, -Infinity, Infinity)
 };
+
