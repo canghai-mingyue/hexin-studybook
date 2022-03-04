e882e79f06a1226c0ccd3db6174e371ed29709b0
canghai-mingyue <939877376@qq.com>
2022-03-04 15:37:27 +0800

    字节面试

--- interviewSubject/subject.js
+++ interviewSubject/subject.js
@@ -843,3 +843,33 @@ const imgLazyLoad = function() {
 // 这里最好加上防抖处理
 document.addEventListener('scroll', imgLazyLoad)
 
+
+// eventBus实现
+class EventBus {
+    constructor(){
+        this.eventContainer = this.eventContainer || new Map() //用一个容器存放事件
+    }
+    on(type,callback){
+        if(!this.eventContainer.has(type)){
+            //如果容器里面没有这种类型的事件，就增加
+            this.eventContainer.set(type,callback)
+
+        }
+    }
+    off(type){
+        if(this.eventContainer.has(type)){
+
+            this.eventContainer.delete(type)
+
+        }
+    }
+    emit(type){
+        let fn = this.eventContainer.get(type)
+        fn.apply(this,[...arguments].slice(1))
+    }
+}
+
+let ev = new EventBus()
+ev.on('myevent',name=>{console.log('hello,',name)})
+ev.emit('myevent','jack')
+
