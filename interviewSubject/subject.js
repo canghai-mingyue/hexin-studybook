a65b695492fed30875616af78c5429d15d68ef77
canghai-mingyue <939877376@qq.com>
2022-04-14 22:16:17 +0800

    乐读一面

--- interviewSubject/subject.js
+++ interviewSubject/subject.js
@@ -357,6 +357,27 @@ Promise.race = function(promiseArr) {
         })
     })
 }
+// Promise.allSettled
+if (!Promise.allSettled) {
+    Promise.allSettled = function (promises) {
+        return new Promise(resolve => {
+            const data = [], len = promises.length;
+            let count = len;
+            for (let i = 0; i < len; i += 1) {
+                const promise = promises[i];
+                promise.then(res => {
+                    data[i] = { status: 'fulfilled', value: res };
+                }, error => {
+                    data[i] = { status: 'rejected', reason: error };
+                }).finally(() => { // promise has been settled
+                    if (!--count) {
+                        resolve(data);
+                    }
+                });
+            }
+        });
+    }
+}
 
 
 // 11.Ajax的实现
